73627b8ca65a0392a14f56099ccf3b14f26498ea
gogomca <gogomca@gmail.com>
2024-07-11 19:59:24 +0200

    Config connection string and database name as properties

--- src/main/java/es/gogomca/javamongodb/App.java
+++ src/main/java/es/gogomca/javamongodb/App.java
@@ -13,23 +13,17 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
-/**
- * Hello world!
- *
- */
 public class App {
 	private static Logger logger = Logger.getLogger(App.class.getName());
 
 	public static void main(String[] args) {
-		// We expect the password as the first execution argument.
-		String user = args[0];
-		String password = args[1];
+		// We expect the connection string and the database name as properties.
+		var connectionString = System.getProperty("connectionString");
+		logger.info(String.format("Connection string: %s", connectionString));
 
-		logger.info(String.format("Credentials: %s - %s", user, password));
+		var databaseName = System.getProperty("databaseName");
+		logger.info(String.format("Database name: %s", databaseName));
 
-		// Generate the connection string to MongoDb database instance in Atlas.
-		var connectionStringTemplate = "mongodb+srv://%s:%s@cluster0.k8t8jwp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-		var connectionString = connectionStringTemplate.formatted(user, password);
 		var mongoConnectionString = new ConnectionString(connectionString);
 
 		// Generate the API to connect to Atlas.
@@ -41,8 +35,8 @@ public class App {
 		// Create a new client and connect to Atlas.
 		try (MongoClient mongoClient = MongoClients.create(settings)) {
 			try {
-				// Send a ping to confirm a successful connection to one of the existing databases from samples.
-				MongoDatabase database = mongoClient.getDatabase("sample_analytics");
+				// Send a ping to confirm a successful connection to one of the existing databases.
+				MongoDatabase database = mongoClient.getDatabase(databaseName);
 				database.runCommand(new Document("ping", 1));
 				logger.info("Pinged your deployment. You successfully connected to MongoDB!");
 			} catch (MongoException e) {
