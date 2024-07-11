7997d10ca1f74bbf3475acf14cce91f963394cae
gogomca <gogomca@gmail.com>
2024-07-11 18:40:39 +0200

    Initial commit
    Synchronous Mongo DB driver dependency.

---
+++ src/test/java/es/gogomca/javamongodb/AppTest.java
@@ -0,0 +1,14 @@
+package es.gogomca.javamongodb;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
